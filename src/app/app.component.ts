0faf9d933a5df68243ee5904c481360235f51dfe
Pradnya Kulkarni <hellopradnya0509@gmail.com>
2024-07-15 17:53:51 -0400

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'sidebar-ng';
+  left_nav_list_item:Array<{title:string,icon:any}>=[
+    {
+      title:'books',
+      icon: document
+    },
+    {
+      title:'Users',
+      icon:document
+    },
+    {
+      title:'test1',
+      icon:document
+    }
+  ]
+
+  showLeftNav:boolean=true;
+
+  showAndHideLeftNav(){
+    this.showLeftNav=!this.showLeftNav;
+  }
+
 }
